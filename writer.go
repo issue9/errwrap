5879de265029c6ed57f3ef03a76b34d7d656071d
caixw <xnotepad@gmail.com>
2020-06-06 12:27:54 +0800

    feat: 添加 Writer

--- writer.go
+++ writer.go
@@ -5,8 +5,15 @@ package errwrap
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"strings"
 )
+
+// Writer 对 io.WrWriter 的简单封闭
+type Writer struct {
+	io.Writer
+	Err error
+}
 
 // Buffer 提供缓存错误的 bytes.Buffer
 type Buffer struct {
@@ -18,6 +25,58 @@ type Buffer struct {
 type StringBuilder struct {
 	strings.Builder
 	Err error
+}
+
+// WString 写入字符串
+func (w *Writer) WString(str string) *Writer {
+	return w.WBytes([]byte(str))
+}
+
+// WByte 写入单个字节内容
+func (w *Writer) WByte(b byte) *Writer {
+	return w.WBytes([]byte{b})
+}
+
+// WBytes 写入字节内容
+func (w *Writer) WBytes(data []byte) *Writer {
+	if w.Err == nil {
+		_, w.Err = w.Write(data)
+	}
+	return w
+}
+
+// WRune 写入单个字节内容
+func (w *Writer) WRune(r rune) *Writer {
+	return w.WBytes([]byte(string(r)))
+}
+
+// WRunes 写入字节内容
+func (w *Writer) WRunes(rs []rune) *Writer {
+	return w.WBytes([]byte(string(rs)))
+}
+
+// Print 相当于 fmt.Fprint(builder, v...)
+func (w *Writer) Print(v ...interface{}) *Writer {
+	if w.Err == nil {
+		_, w.Err = fmt.Fprint(w, v...)
+	}
+	return w
+}
+
+// Println 相当于 fmt.Fprintln(builder, v...)
+func (w *Writer) Println(v ...interface{}) *Writer {
+	if w.Err == nil {
+		_, w.Err = fmt.Fprintln(w, v...)
+	}
+	return w
+}
+
+// Printf 相当于 fmt.Fprintf(builder, format, v...)
+func (w *Writer) Printf(format string, v ...interface{}) *Writer {
+	if w.Err == nil {
+		_, w.Err = fmt.Fprintf(w, format, v...)
+	}
+	return w
 }
 
 // WString 写入字符串
--- writer_test.go
+++ writer_test.go
@@ -3,10 +3,25 @@
 package errwrap
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/issue9/assert"
 )
+
+func TestWriter(t *testing.T) {
+	a := assert.New(t)
+
+	buf := new(bytes.Buffer)
+	w := &Writer{
+		Writer: buf,
+	}
+	w.WString("123").WRunes([]rune("456")).WByte('7').
+		Printf("%s", "89").Print("10").Println()
+
+	a.Equal(buf.String(), "12345678910\n").
+		NotError(w.Err)
+}
 
 func TestBuffer(t *testing.T) {
 	a := assert.New(t)
@@ -22,10 +37,10 @@ func TestBuffer(t *testing.T) {
 func TestStringBuilder(t *testing.T) {
 	a := assert.New(t)
 
-	buf := &StringBuilder{}
-	buf.WString("123").WRunes([]rune("456")).WByte('7').
+	builder := &StringBuilder{}
+	builder.WString("123").WRunes([]rune("456")).WByte('7').
 		Printf("%s", "89").Print("10").Println()
 
-	a.Equal(buf.String(), "12345678910\n").
-		NotError(buf.Err)
+	a.Equal(builder.String(), "12345678910\n").
+		NotError(builder.Err)
 }
